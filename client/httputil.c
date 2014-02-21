c26cac51a2a317931fa0876978dc8dbe443b409c
linzhiqi <linzhiqi07@gmail.com>
2014-02-21 11:36:38 +0200

    1.add writen() so write in loop. 2.improve fetch body, works if response code, content length or start place of body is not get in first read buffer. 3.upload_file use select() so whenever server sends something, we can read it and handle it instead of blocking in writting data to server.

--- client/httputil.c
+++ client/httputil.c
@@ -9,6 +9,7 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <math.h>
 
 #define MAXFILENAME 100
 #define MAXHOSTNAME 200
@@ -17,6 +18,18 @@
 #define MAX_LOCATION_LEN 200
 #define MAXMSGBUF 5000
 #define DEFAULT_FILE_NAME "downloaded_file"
+#ifndef max
+#define max(a,b)            (((a) < (b)) ? (b) : (a))
+#endif
+
+ssize_t writen(int fd, const char *buf, size_t len);
+char *getFileFromRes(char * file_name, const char * res_location);
+int getofd(const char * res_location, const char *local_path, int store_data);
+int parserespcode(char ** pptr);
+long parselength(char ** pptr);
+int parsebodystart(char ** pptr);
+
+
 
 /* url:=[protocol://]+<hostname>+[:port number]+[resource location] */
 void parse_url(const char *url, char *port, char *host, char *location){
@@ -52,19 +65,6 @@ void parse_url(const char *url, char *port, char *host, char *location){
     return;
 }
 
-/*
- *get the part after the last '/' as file name
- *if nothing after the last '/', return NULL
- */
-char *getFileFromRes(char * file_name, const char * res_location){
-    char * ptr;
-    if(res_location==NULL) return NULL;
-    
-    ptr=strrchr(res_location, '/');
-    if(*(ptr++)=='\0') return NULL;
-    strcpy(file_name, ptr);
-    return file_name;
-}
 
 /*
  *host - tageted host name or ip address, support both ipv4 and ipv6
@@ -111,228 +111,285 @@ int tcp_connect(const char *host, const char *serv)
 	return(sockfd);
 }
 
+
+
 /*
  *create and send http GET method to fetch body data
  *fetched data is stored into local file while store_data==1 or just be printed to console if store_data==0
  */
 int fetch_body(int sockfd, char * res_location, const char * hostName, const char *local_path, int store_data)
 {
-	long int n=0,total=0,len=0,n1=0,code=0;
-	char httpMsg[MAXMSGBUF];
-	char *tmp;
-        const char *fileName;
-	int fd,cnt=0;
-	/*in case connection has closed*/
-	if(sockfd==-1)
-	{
-		printf("no site is connected, please connect first\n");
-		return 0;
-	}
+    long int n=0,total=0,len=-1,code=0;
+    char httpMsg[MAXMSGBUF];
+    char *ptr;
+    int fd,bodystartfl=0,ofdgotfl=0;
+    size_t tobewritten;
 
-	sprintf(httpMsg,"GET %s HTTP/1.1\r\nHost:%s\r\nIam: linzhiqi\r\n\r\n",res_location,hostName);
+    sprintf(httpMsg,"GET %s HTTP/1.1\r\nHost:%s\r\nIam: linzhiqi\r\n\r\n",res_location,hostName);
+    if(writen(sockfd,httpMsg,strlen(httpMsg))!=strlen(httpMsg))
+    {
+        printf("GET message is not sent completely\n");
+        close(sockfd);
+        return -1;
+    }
 
-	if((n=write(sockfd,httpMsg,strlen(httpMsg)))<0)
-	{
-		perror("read error");
-		return -1;
-	}
-	if(n<strlen(httpMsg))
-	{
-		printf("GET message is not sent completely\n");
-		return -1;
-	}
-	/*because some string manipulation is going to take on the buffer, have to mark '\0' following the end of valuable part
-	set all byte '\0' so that received part always followed by a '\0'. */
-	memset(httpMsg,0,MAXMSGBUF);
-	/*parse the http header on the first received part(4999 bytes). assume content-type and empty line will be found here*/
-	if((n = read(sockfd, httpMsg, MAXMSGBUF-1))>0)
-	{
-		printf("get body data in first read(): %ld\n",n);
-		/*strstr() returns the pointer pointing to the first char of substring "HTTP/1.1"*/
-		tmp=strstr(httpMsg,"HTTP/1.1");
-		tmp+=8;
-		/*get the response code*/
-		code=strtol(tmp,&tmp,10);
-		if(code!=200)
-		{
-			printf("Response not OK, code: %ld\n",code);
-			return -1;
-		}
-		tmp=strstr(httpMsg,"Content-Length");
-		tmp+=15;
-		/*get the value of content-length*/
-		len=strtol(tmp,&tmp,10);
-		printf("length:%ld\n",len);
-		tmp=strstr(tmp,"\r\n\r\n");
-		/*tmp is pointing at the beginning of response body*/
-		tmp+=4;
-		/*total currently = how many bytes of body data in this buffer needed to be read*/
-		total=(httpMsg+n)-tmp;
+    memset(httpMsg,0,MAXMSGBUF);
+    while((n = read(sockfd, httpMsg, MAXMSGBUF-1))>0){
+        ptr=httpMsg;
+        if(code==0){
+            code=parserespcode(&ptr);
+        }
+        if(len==-1){
+            len=parselength(&ptr);
+        }
+        if(bodystartfl==0){
+            bodystartfl=parsebodystart(&ptr);
+        }
+        if(code==200 && len>0 && bodystartfl){
+            if(ofdgotfl==0){
+	        if((fd=getofd(res_location,local_path,store_data))==-1) return -1;
+                ofdgotfl=1;
+            }
+            tobewritten=strlen(ptr);
+            if(writen(fd,ptr,tobewritten)!=tobewritten)
+            {
+                printf("Received body content is not written completely\n");
+                close(fd);
+                return -1;
+            }
+            total+=tobewritten;
+            if(total>=len)
+	    {
+	        close(fd);
+                return 0;
+	    }
+        }
+        memset(httpMsg,0,MAXMSGBUF);
+    }
 
-                if(store_data){
-		    /*obtain the file name from resource location, if not specified*/
-                    if(local_path==NULL || strcmp(local_path,"")==0){
-                        char buf[MAXFILENAME];
-                        if(getFileFromRes(buf, res_location)==NULL) fileName=DEFAULT_FILE_NAME;
-                        else fileName=buf;
-                    }else{
-                        fileName=local_path;
-                    }
-
-		    if((fd=creat(fileName,S_IRWXO|S_IRWXG|S_IRWXU))==-1)
-		    {
-			    perror("file create error");
-			    return -1;
-		    }
-                }else{
-                    fd=1;
-                }
-
-		/*cnt = how many chars needs to write into local file*/
-		cnt=(int)total;
-		while((n1=write(fd,tmp,(size_t)cnt))>=0)
-		{
-			if(n1<cnt)
-			{
-				tmp+=n1;
-				cnt-=n1;
-			}else{
-				break;
-			}
-		}
-		/*if message ends in the first 4999 bytes*/
-		if(total>=len)
-		{
-			close(fd);
-			return 0;
-		}
-	}
-
-	if(n<0)
-	{
-		perror("read error");
-		return -1;
-	}else if (n==0){
-		printf("connection is closed!\n");
-		return -1;
-	}
-
-	memset(httpMsg,0,MAXMSGBUF);
-	/*if message doesn't end in the first 4999, keep read from the socket*/
-	while ( (n = read(sockfd, httpMsg, MAXMSGBUF-1)) > 0) 
-	{		
-		cnt=n;
-		tmp=httpMsg;
-		while((n1=write(fd,tmp,(size_t)cnt))>=0)
-		{
-			if(n1<cnt)
-			{
-				tmp+=n1;
-				cnt-=n1;
-			}else{
-				break;
-			}
-		}
-		total+=n;
-		if(total>=len)
-		{
-			close(fd);
-			return 0;
-		}
-		memset(httpMsg,0,MAXMSGBUF);
-	}
-
-	if (n <0) {
-		perror("read error");
-		return -1;
-	}else if (n==0){
-		printf("connection is closed!\n");
-		return -1;
-	}
-	return 0;
+    if(n<0)
+    {
+	perror("read error");
+	return -1;
+    }else if (n==0){
+	printf("connection is closed by server, however body is not received completely.\n");
+	return -1;
+    }
+    return 0;
 }
+
+int sendfile(int fd, int sockfd){
+    fd_set rset;
+    int maxfdp1, stdineof, code, n;
+    char buf[MAXMSGBUF], *ptr;
+    stdineof = 0;
+    FD_ZERO(&rset);
+    for ( ; ; ) {
+        if (stdineof == 0)
+            FD_SET(fd, &rset);
+        FD_SET(sockfd, &rset);
+        maxfdp1 = max(fd, sockfd) + 1;
+        
+        select(maxfdp1, &rset, NULL, NULL, NULL);
+
+        if (FD_ISSET(sockfd, &rset)) {	/* socket is readable */
+            if ( (n = read(sockfd, buf, MAXMSGBUF)) == 0) {
+                if (stdineof == 1){
+                    printf("local file written to socket completely, however server closed tcp without HTTP response\n");
+                    return 0;		/* normal termination */
+                }
+		else{
+		    perror("sendfile: server terminated prematurely");
+                    exit(-1);
+                }   
+	    }
+
+            if(n<0){
+                perror("sendfile: read error");
+                exit(-1);
+            }
+
+            ptr = buf;
+            code=parserespcode(&ptr);
+            if((code/100)==2)
+            {
+		printf("Put respond OK\n");
+		return 0;
+            }else{
+		printf("Put respond not OK\n");
+		return -1;
+            }
+	}
+
+	if (FD_ISSET(fd, &rset)) {  /* input is readable */
+            if ( (n = read(fd, buf, MAXMSGBUF)) == 0) {
+                stdineof = 1;
+		FD_CLR(fd, &rset);
+		continue;
+            }
+            if(writen(sockfd,buf,n)!=n)
+            {
+                perror("writing local file data to socket failed");
+                close(fd);
+                exit(-1);
+            }
+        }
+    }
+}
+
 
 /*
  *create PUT method and send it with body data read from local file
  */
 int upload_file(int sockfd, const char * res_location, const char * hostName, const char *local_path)
 {
-	long int n=0,len=0,n1=0,code=0;
-	char httpMsg[MAXMSGBUF];
-	char *tmp=NULL;
-	int fd;
-	struct stat fileinfo;
-	if(sockfd==-1)
-	{
-		printf("no site is connected, please connect first\n");
-		return 0;
-	}
+    long int len=0;
+    char httpMsg[MAXMSGBUF];
+    int fd;
+    
+    struct stat fileinfo;
 
-	/*get the file size of the object*/
-	if(stat(local_path, &fileinfo)==-1)
-	{
-		perror("file info error");
-		return -1;
-	}
-	len=(long int)fileinfo.st_size;
+    /*get the file size of the object*/
+    if(stat(local_path, &fileinfo)==-1)
+    {
+        perror("file info error");
+        return -1;
+    }
+    len=(long int)fileinfo.st_size;
 
-	sprintf(httpMsg,"PUT %s HTTP/1.1\r\nHost:%s\r\nIam: linzhiqi\r\nContent-Length: %ld\r\n\r\n",res_location,hostName,len);
-	if((n=write(sockfd,httpMsg,strlen(httpMsg)))<0)
-	{
-		perror("read error");
-		return -1;
-	}
+    sprintf(httpMsg,"PUT %s HTTP/1.1\r\nHost:%s\r\nIam: linzhiqi\r\nContent-Length: %ld\r\n\r\n",res_location,hostName,len);
+
+    if(writen(sockfd,httpMsg,strlen(httpMsg))!=strlen(httpMsg))
+    {
+        printf("PUT message is not sent completely\n");
+        close(sockfd);
+        return -1;
+    }
 	
+    if((fd=open(local_path,O_RDWR))==-1)
+    {
+        perror("open local file");
+        return -1;
+    }
 
-	if((fd=open(local_path,O_RDWR))==-1)
-	{
-		perror("open error");
-		return -1;
-	}
-	/*read file into buffer, then to socket, block by block*/
-	while((n1=read(fd,httpMsg,MAXMSGBUF))>0)
-	{
-		tmp=httpMsg;
-		while((n=write(sockfd,tmp,n1))>=0)
-		{
-                        printf("read data len=%ld, write data len=%ld\n",n1,n);
-			if(n<n1)
-			{
-				tmp+=n;
-				n1-=n;
-			}else{
-				break;
-			}
-		}
-		if(n<0){
-			perror("write body");
-			return -1;
-		}
-	}
-	/*check the response from server*/
-	if((n = read(sockfd, httpMsg, MAXMSGBUF-1))>0)
-	{
-		tmp=strstr(httpMsg,"HTTP/1.1");
-		tmp+=8;
-		code=strtol(tmp,&tmp,10);
-		if((code/100)==2)
-		{
-			printf("Put respond OK\n");
-			return 0;
-		}else{
-			printf("Put respond not OK\n");
-			return -1;
-		}
-	}
-	if(n1==0)
-	{
-		printf("connection is closed!\n");
-		return -1;
-	}else if(n1==-1){
-		perror("write to site error");
-		return -1;
-	}
+    return sendfile(fd, sockfd);
+}
 
-	
-	return 0;
+
+/*
+ * write the first len of bytes in buf to fd. It writes again if not all len of bytes are written.
+ * error can happen, in this case, it returns how many bytes have been written and shows error info.
+ */
+ssize_t writen(int fd, const char *buf, size_t len){
+    int cnt, n, total;
+    const char * ptr=buf;
+    cnt = len;
+    while((n=write(fd,ptr,cnt))>=0)
+    {
+	if(n<cnt)
+	{
+	    ptr+=n;
+            total+=n;
+	    cnt-=n;
+	}else{
+	    return len;
+	}
+    }
+    perror("write error");
+    return total;
+}
+
+/*
+ *get the part after the last '/' as file name
+ *if nothing after the last '/', return NULL
+ */
+char *getFileFromRes(char * file_name, const char * res_location){
+    char * ptr;
+    if(res_location==NULL) return NULL;
+    
+    ptr=strrchr(res_location, '/');
+    if(*(ptr++)=='\0') return NULL;
+    strcpy(file_name, ptr);
+    return file_name;
+}
+
+/*
+ * return the file descriptor according to the parameters
+ * the logic is if store_data==0, it returns 1 which is the standart output.
+ * if store_data==1, it create a local file specified by argument local_path, and returns the file descriptor of it.
+ * if local_path is empty or NULL, it use argument res_location, the resource part of a url, to determine the file name.
+ */
+int getofd(const char * res_location, const char *local_path, int store_data){
+    const char * filename;
+    int fd;
+    if(store_data==1){
+	/*obtain the file name from resource location, if not specified*/
+        if(local_path==NULL || strcmp(local_path,"")==0){
+            char buf[MAXFILENAME];
+            if(getFileFromRes(buf, res_location)==NULL) filename=DEFAULT_FILE_NAME;
+            else filename=buf;
+        }else{
+            filename=local_path;
+        }
+
+        if((fd=creat(filename,S_IRWXO|S_IRWXG|S_IRWXU))==-1)
+	{
+	    perror("file create error");
+	    return -1;
+        }
+    }else{
+        fd=1;
+    }
+    return fd;
+}
+
+/*
+ * return the response code if it's found in the string *pptr, and move its starting place to the first char after the code. 
+ * if code string is not found in the string, return 0 and leave the string un-changed.
+ */
+int parserespcode(char ** pptr){
+    int code=0;
+    char * ptr;
+    ptr=strstr(*pptr,"HTTP/1.1");
+    if(ptr==NULL){
+        return code;
+    }
+    ptr+=8;
+    /*get the response code*/
+    code=(int)strtol(ptr,&ptr,10);
+    *pptr=ptr;
+    return code;
+}
+
+/*
+ * return the value of Content_Length if it's found in the string *pptr, and move its starting place to the first char after the length value. 
+ * if Content-Length header string is not found in the string, return -1 and leave the string un-changed.
+ */
+long parselength(char ** pptr){
+    long len=-1;
+    char * ptr;
+    ptr=strstr(*pptr,"Content-Length");
+    if(ptr==NULL){
+        return len;
+    }
+    ptr+=15;
+    len=strtol(ptr,&ptr,10);
+    *pptr=ptr;
+    return len;
+}
+
+/*
+ * return 1 if the starting place of http body is found in the string *pptr, and move its starting place to the first char after the blank line. 
+ * if the blank line is not found in the string, return 0 and leave the string un-changed.
+ */
+int parsebodystart(char ** pptr){
+    char * ptr;
+    ptr=strstr(*pptr,"\r\n\r\n");
+    if(ptr==NULL){
+        return 0;
+    }else{
+        ptr+=4;
+        *pptr=ptr;
+        return 1;
+    }
 }
